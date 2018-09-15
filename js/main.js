484290510dea76ed1e804a98fe46da600d364ade
Misbauddin Chowdhury Shahan <shahan953@hotmail.com>
2018-09-15 19:14:22 +0600

    tailor

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".home-slides").owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        touchDrag: false,
+        mouseDrag: false,
+        autoplay: true,
+        smartSpeed: 500,
+        loop: true,
+        navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+    }); 
+});
